c4009031fa1b49ceda8674ca30de26ef63209370
zzarr <azharsyah.curug@gmail.com>
2024-07-08 14:43:10 +0700

    add head and script

---
+++ devgen-company-profile/resources/views/layout/head-landingpage.blade.php
@@ -0,0 +1,27 @@
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <!-- Bootstrap CSS -->
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/bootstrap.min.css') }} ">
+ <!-- Owl Carousel CSS -->
+ <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+ <!-- Owl Default CSS -->
+ <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+ <!-- Owl Magnific CSS -->
+ <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+ <!-- Animate CSS -->
+ <link rel="stylesheet" href="assets/css/animate.min.css">
+ <!-- Boxicons CSS -->
+ <link rel="stylesheet" href="assets/css/boxicons.min.css">
+ <!-- Flaticon CSS -->
+ <link rel="stylesheet" href="assets/css/flaticon.css">
+ <!-- Meanmenu CSS -->
+ <link rel="stylesheet" href="assets/css/meanmenu.css">
+ <!-- Odometer CSS-->
+ <link rel="stylesheet" href="assets/css/odometer.min.css">
+ <!-- Style CSS -->
+ <link rel="stylesheet" href="assets/css/style.css">
+ <!-- RTL CSS -->
+ <link rel="stylesheet" href="assets/css/dark.css">
+ <!-- Responsive CSS -->
+ <link rel="stylesheet" href="assets/css/responsive.css">
---
+++ devgen-company-profile/resources/views/layout/script-landingpage.blade.php
@@ -0,0 +1,26 @@
+ <!-- dark version -->
+
+ <!-- Jquery Slim JS -->
+ <script src="assets/js/jquery.min.js"></script>
+ <!-- Bootstrap JS -->
+ <script src="assets/js/bootstrap.bundle.min.js"></script>
+ <!-- Meanmenu JS -->
+ <script src="assets/js/jquery.meanmenu.js"></script>
+ <!-- Owl Carousel JS -->
+ <script src="assets/js/owl.carousel.min.js"></script>
+ <!-- Magnific JS -->
+ <script src="assets/js/jquery.magnific-popup.min.js"></script>
+ <!-- Appear JS -->
+ <script src="assets/js/jquery.appear.min.js"></script>
+ <!-- Odometer JS -->
+ <script src="assets/js/odometer.min.js"></script>
+ <!-- Form Ajaxchimp JS -->
+ <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+ <!-- Form Validator JS -->
+ <script src="assets/js/form-validator.min.js"></script>
+ <!-- Contact JS -->
+ <script src="assets/js/contact-form-script.js"></script>
+ <!-- Wow JS -->
+ <script src="assets/js/wow.min.js"></script>
+ <!-- Custom JS -->
+ <script src="assets/js/main.js"></script>

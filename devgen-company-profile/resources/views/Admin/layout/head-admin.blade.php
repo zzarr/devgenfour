1607925d96c10f1cd93e4bf65ae22d5e6603bf91
zzarr <azharsyah.curug@gmail.com>
2024-07-11 20:49:00 +0700

    up

---
+++ devgen-company-profile/resources/views/Admin/layout/head-admin.blade.php
@@ -0,0 +1,3 @@
+<link href="{{ asset('metrica/dist/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('metrica/dist/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('metrica/dist/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
---
+++ devgen-company-profile/resources/views/Admin/layout/script-admin.blade.php
@@ -0,0 +1,11 @@
+ <!-- Javascript  -->
+ <!-- vendor js -->
+
+ <script src="{{ asset('metrica/dist/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <script src="{{ asset('metrica/dist/assets/libs/simplebar/simplebar.min.js') }}"></script>
+ <script src="{{ asset('metrica/dist/assets/libs/feather-icons/feather.min.js') }}"></script>
+
+ <script src="{{ asset('metrica/dist/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+ <script src="{{ asset('metrica/dist/assets/js/pages/analytics-index.init.js') }}"></script>
+ <!-- App js -->
+ <script src="{{ asset('metrica/dist/assets/js/app.js') }}"></script>

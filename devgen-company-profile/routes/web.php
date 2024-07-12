81e3337f6edb8b02f812e53feef1c16d8be0e6cc
laelatulkna <laelatulkna2103@gmail.com>
2024-07-12 10:20:41 +0700

    test

--- devgen-company-profile/routes/web.php
+++ devgen-company-profile/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('landing-page', function () {
+    return view('landing-page/why_choose_us');
+});
+
+Route::get('/', [LandingPageController::class, 'index']);

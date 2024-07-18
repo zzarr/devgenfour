e288d1d3fa6b95a62ec8022b9ba4a5e8ecb7bc0e
luckydxd <luckydawamuzdzikri@gmail.com>
2024-07-18 21:27:57 +0700

    datatables partner

--- devgen-company-profile/app/Http/Controllers/admin/PartnerController.php
+++ devgen-company-profile/app/Http/Controllers/admin/PartnerController.php
@@ -24,7 +24,9 @@ class PartnerController extends Controller
             })
             ->addColumn('action', function($row) {
                 $editUrl = route('editpartner_admin', $row->id_partner);
-                return '<a href="'.$editUrl.'" class="btn btn-outline-info btn-icon-circle btn-icon-circle-sm"><i class="ti ti-pencil"></i></a>';
+                $deleteUrl = route('deletepartner_admin', $row->id_partner);
+                return '<a href="'.$editUrl.'" class="btn btn-outline-info btn-icon-circle btn-icon-circle-sm"><i class="ti ti-pencil"></i></a>
+                        <button data-url="'.$deleteUrl.'" class="btn btn-outline-danger btn-icon-circle btn-icon-circle-sm btn-delete"><i class="ti ti-trash"></i></button>';
             })
             ->rawColumns(['image', 'action'])
             ->make(true);
@@ -67,24 +69,48 @@ class PartnerController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit()
+    public function edit($id)
     {
-        return view('Admin.partneredit');
+        $partner = Partners::findOrFail($id);
+        return view('Admin.partneredit', compact('partner'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $data = $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $partner = Partners::findOrFail($id);
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $filename = time() . '_' . $file->getClientOriginalName();
+            $file->move(public_path('partners'), $filename);
+            $data['image'] = $filename;
+        }
+
+        $partner->update($data);
+
+        return redirect()->route('partner_admin')
+            ->with('success', 'Partner updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $partner = Partners::findOrFail($id);
+        if (file_exists(public_path('partners/'.$partner->image))) {
+            unlink(public_path('partners/'.$partner->image));
+        }
+        $partner->delete();
+
+        return redirect()->route('partner_admin')
+            ->with('success', 'Partner deleted successfully.');
     }
 }

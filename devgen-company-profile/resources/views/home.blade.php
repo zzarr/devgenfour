327ef15fa4dd9f1912400e5fbdee7026038839e0
Legi Afatah <legi.gaya@gmail.com>
2024-07-18 10:56:46 +0700

    modifikasi head dan script

--- devgen-company-profile/resources/views/home.blade.php
+++ devgen-company-profile/resources/views/home.blade.php
@@ -1,142 +1,18 @@
 <!DOCTYPE html>
 <html lang="zxx" class="theme-light">
-   
-
-    @include('layout.head-landingpage')
-
+   <head> @include('layout.head-landingpage')</head>
     <body>
-
         <!-- Start Navbar Area -->
-        <div class="navbar-area">
-            <div class="fria-responsive-nav">
-                <div class="container">
-                    <div class="fria-responsive-menu">
-                        <div class="logo">
-                            <a href="index.html">
-                                <img
-                                    src="img/dev.png"
-                                    class="black-logo"
-                                    alt="image"
-                                />
-                                <img
-                                    src="img/dev.png"
-                                    class="white-logo"
-                                    alt="image"
-                                />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="fria-nav">
-                <div class="container">
-                    <nav class="navbar navbar-expand-md navbar-light">
-                        <a class="navbar-brand" href="index.html">
-                            <img
-                                src="img/dev.png"
-                                class="black-logo"
-                                alt="image"
-                            />
-                            <img
-                                src="img/dev.png"
-                                class="white-logo"
-                                alt="image"
-                            />
-                        </a>
-
-                        <div
-                            class="collapse navbar-collapse mean-menu"
-                            id="navbarSupportedContent"
-                        >
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link active">
-                                        Home
-                                    </a>
-                                    
-
-                                <li class="nav-item">
-                                    <a href="about.html" class="nav-link">
-                                        About
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        Services
-                                    </a>
-                                    
-
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        Projects
-                                    </a>
-                                    
-
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        Contact
-                                    </a>
-                                </li>
-                            </ul>
-
-                            <div class="others-options">
-                                <div class="option-item">
-                                    <i class="close-btn flaticon-cancel"></i>
-                                    <div class="search-overlay search-popup">
-                                        <div class="search-box">
-                                            <form class="search-form">
-                                                <input
-                                                    class="search-input"
-                                                    name="search"
-                                                    placeholder="Search"
-                                                    type="text"
-                                                />
-
-                                                <button
-                                                    class="search-button"
-                                                    type="submit"
-                                                >
-                                                    <i
-                                                        class="flaticon-search"
-                                                    ></i>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
+        @include('layout.navbar')
         <!-- End Navbar Area -->
-
-        <!-- Sidebar Modal -->
-
-        <!-- End Sidebar Modal -->
 
         <!-- Start Banner Area -->
        @include('layout.hero-section')
         <!-- End Banner Area -->
 
-        <!-- Start Features Area -->
- 
-        <!-- End Features Area -->
-
-        <!-- Start Work Area -->
-     
-        <!-- End work Area -->
-
         <!-- Start Services Area -->
         @include('layout.services-section')
         <!-- End Services Area -->
-
-        <!-- Start Support Area -->
-       
-        <!-- End Support Area -->
 
         <!-- Start Overview Area -->
         @include('layout.overview-area-section')
@@ -162,18 +38,6 @@
         @include('layout.team-area-section')
         <!-- End Team Area -->
 
-        <!-- Start Skills Area -->
-        
-        <!-- End Skills Area -->
-
-        <!-- Start Blog Area -->
-        
-        <!-- End Blog Area -->
-
-        <!-- Start Clients Area -->
-
-        <!-- End Clients Area -->
-
         <!-- Start Partner Section -->
        @include('layout.mediapatner-section')
         <!-- End Partner Section -->
@@ -181,14 +45,6 @@
         <!-- Start Footer Area -->
        @include('layout.footer-section')
         <!-- End Copy Right Area -->
-
-        <!-- Start Go Top Section -->
-        @include('layout.go-top-section')
-        <!-- End Go Top Section -->
-
-        <!-- dark version -->
-        @include('layout.dark-version-section')
-        <!-- dark version -->
 
         <!-- Jquery Slim JS -->
         @include('layout.script-landingpage')
--- devgen-company-profile/resources/views/layout/head-landingpage.blade.php
+++ devgen-company-profile/resources/views/layout/head-landingpage.blade.php
@@ -4,24 +4,24 @@
  <!-- Bootstrap CSS -->
  <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/bootstrap.min.css') }} ">
  <!-- Owl Carousel CSS -->
- <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/owl.carousel.min.css') }}">
  <!-- Owl Default CSS -->
- <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/owl.theme.default.min.css') }}">
  <!-- Owl Magnific CSS -->
- <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/magnific-popup.min.css') }}">
  <!-- Animate CSS -->
- <link rel="stylesheet" href="assets/css/animate.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/animate.min.css') }}">
  <!-- Boxicons CSS -->
- <link rel="stylesheet" href="assets/css/boxicons.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/boxicons.min.css') }}">
  <!-- Flaticon CSS -->
- <link rel="stylesheet" href="assets/css/flaticon.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/flaticon.css') }}">
  <!-- Meanmenu CSS -->
- <link rel="stylesheet" href="assets/css/meanmenu.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/meanmenu.css') }}">
  <!-- Odometer CSS-->
- <link rel="stylesheet" href="assets/css/odometer.min.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/odometer.min.css') }}">
  <!-- Style CSS -->
- <link rel="stylesheet" href="assets/css/style.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/style.css') }}">
  <!-- RTL CSS -->
- <link rel="stylesheet" href="assets/css/dark.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/dark.css') }}">
  <!-- Responsive CSS -->
- <link rel="stylesheet" href="assets/css/responsive.css">
+ <link rel="stylesheet" href="{{ asset('fria/fria/assets/css/responsive.css') }}">

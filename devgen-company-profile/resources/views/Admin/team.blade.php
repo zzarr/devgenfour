26fabc18db8c26c222bf35f88aa50d7da2dac2fe
zzarr <azharsyah.curug@gmail.com>
2024-07-18 21:39:02 +0700

    up app setting

--- devgen-company-profile/resources/views/Admin/team.blade.php
+++ devgen-company-profile/resources/views/Admin/team.blade.php
@@ -18,47 +18,6 @@
     <!-- end page title end breadcrumb -->
 
     <div class="row">
-<<<<<<< HEAD
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">List Team</h4>
-            </div><!--end card-header-->
-            <div class="card-body">
-            <a href="{{ route('addteam_admin') }}" class="btn btn-primary"><i class="ti ti-plus"></i> Add</a>
-                <div class="table-responsive">
-                    
-                    <table class="table" id="datatable_1">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>Name</th>
-                                <th>Ext.</th>
-                                <th>City</th>
-                                <th data-type="date" data-format="YYYY/DD/MM">Start Date</th>
-                                <th>Completion</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Unity Pugh</td>
-                                <td>9958</td>
-                                <td>Curicó</td>
-                                <td>2005/02/11</td>
-                                <td>37%</td>
-                                <td>
-                                <a href="{{ route('editteam_admin') }}" type="button" class="btn btn-outline-info btn-icon-circle btn-icon-circle-sm"><i class="ti ti-pencil"></i></a>
-                                <button type="button" class="btn btn-outline-danger btn-icon-circle btn-icon-circle-sm"><i class="ti ti-trash"></i></button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div><!--end table-responsive-->
-            </div><!--end card-body-->
-        </div><!--end card-->
-    </div><!--end col-12-->
-</div><!--end row-->
-=======
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
@@ -103,5 +62,4 @@
             </div><!--end card-->
         </div><!--end col-12-->
     </div><!--end row-->
->>>>>>> 931cfd448276d58d8b267c69b9b54d5b04d496bc
 @endsection

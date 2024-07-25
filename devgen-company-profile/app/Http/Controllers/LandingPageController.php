4b3ab2d6fc2fa8c7e4c65efcbd25fb6ad2f11df6
Andini2204 <andini0422@gmail.com>
2024-07-25 13:00:21 +0700

    oke

--- devgen-company-profile/app/Http/Controllers/LandingPageController.php
+++ devgen-company-profile/app/Http/Controllers/LandingPageController.php
@@ -16,25 +16,25 @@ class LandingPageController extends Controller
     public function index()
     {
         $appSetting = AppSetting::first();
-<<<<<<< HEAD
+
         $services = Services::first();
-=======
+
         $numbers = AppSetting::all();
->>>>>>> 41c3a9af11dcb9fa78046e5e73830bc5692a19ad
+
         $projects = Project::all();
         $partners = Partners::all();
         $team = Team::all();
         return view('home', compact('appSetting', 'projects','services','partners','team'));
     }
 
-<<<<<<< HEAD
+
     public function edit($id)
     {
 
         return view('admin.app_setting', compact('settings'));
-=======
+
         return view('home', compact('appSetting', 'projects', 'aboutUs', 'numbers'));
->>>>>>> 41c3a9af11dcb9fa78046e5e73830bc5692a19ad
+
     }
 
     public function about()

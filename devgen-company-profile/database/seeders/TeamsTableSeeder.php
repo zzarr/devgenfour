f39ea53d4fd7d226518d9620993c3609c1d5b37e
zzarr <azharsyah.curug@gmail.com>
2024-07-29 10:05:12 +0700

    up seeder

--- devgen-company-profile/database/seeders/TeamsTableSeeder.php
+++ devgen-company-profile/database/seeders/TeamsTableSeeder.php
@@ -15,6 +15,9 @@ class TeamsTableSeeder extends Seeder
             'name' => 'John Doe',
             'jabatan' => 'Team Leader',
             'foto' => 'dummy_thumbnail1',
+            'facebook' => 'https://facebook.com',
+            'instagram' => 'https://instagram.com',
+            'linkedin' => 'https://linkedin.com',
             'created_at' => now(),
             'updated_at' => now(),
         ]);

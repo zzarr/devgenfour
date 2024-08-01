7040fc9d10e5e7bc1886ef91bd3d81d052fd28fb
fadhilar22 <fadhilanugrah864@gmail.com>
2024-08-01 09:43:44 +0700

    new

--- devgen-company-profile/resources/views/layout/whychooseus-section.blade.php
+++ devgen-company-profile/resources/views/layout/whychooseus-section.blade.php
@@ -1,23 +1,35 @@
-<section class="choose-section pt-100 pb-70">
+<section class="choose-section ptb-100">
     <div class="container">
         <div class="section-title">
             <h2>Why Choose Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
             <div class="bar"></div>
         </div>
-        
+
         <div class="row align-items-center">
-            @foreach($choose as $item)
-                <div class="col-lg-4 col-md-6">
-                    <div class="choose-text">
-                        <div class="icon">
-                            
-                            <img src="{{ asset('choose/'. $item->icon) }}" alt="{{ $item->title }}" style="width: 70px; height: 70px; border-radius: 50%; object-fit: cover;">
+            <!-- Konten dinamis -->
+            <div class="col-lg-8 col-md-6">
+                <div class="choose-content-wrapper">
+                    @foreach($choose as $item)
+                        <div class="choose-content">
+                            <div class="icon">
+                                <img src="{{ asset('choose/'. $item->icon) }}" alt="{{ $item->title }}" style="width: 70px; height: 70px; border-radius: 50%; object-fit: cover;">
+                            </div>
+                            <div class="text-content">
+                                <h3>{{ $item->title }}</h3>
+                                <p>{{ $item->description }}</p>
+                            </div>
                         </div>
-                        <h3>{{ $item->title }}</h3>
-                        <p>{{ $item->description }}</p>
-                    </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
+
+            <!-- Gambar statis -->
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-image">
+                    <img src="assets/img/why-choose-us.png" alt="image">
+                </div>
+            </div>
         </div>
     </div>
 </section>
